d9acca2be5a967850da2fcece48789d76a48d174
Kwasi Asiedu <createdarko@gmail.com>
2023-01-25 08:32:37 +0000

    rebase

--- src/pages/api/research.ts
+++ src/pages/api/research.ts
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { main } from '@/backend'
+//import { main } from '@/backend'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 // type Data = {
@@ -10,13 +10,14 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
-  main({
-    query:  req.query.q as string,
-    start: req.query.start as string,
-    onComplete: function (data: any): void { 
-      res.status(200).json(data)
-    }
-})
+  res.status(200).json({status: 'okay'})
+//   main({
+//     query:  req.query.q as string,
+//     start: req.query.start as string,
+//     onComplete: function (data: any): void { 
+//       res.status(200).json(data)
+//     }
+// })
 
   
 }
